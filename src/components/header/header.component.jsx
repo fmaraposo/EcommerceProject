042f6b9054017e977d9379176bb37ddc7035995a
Francisco Martins <fmaraposo@gmail.com>
2021-01-28 17:54:42 +0000

    cart dropdown menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -14,18 +16,24 @@ const Header = ({currentUser}) => {
       </Link>
       <div className="options">
         <Link className="option" to="/shop">
-            SHOP
+          SHOP
         </Link>
         <Link className="option" to="/shop">
-            CONTACT
+          CONTACT
         </Link>
-        {
-          currentUser ? 
-          <div className="option" onClick={() => auth.signOut()}>SIGN OUT </div>
-          :
-          <Link className="option" to="/signin"> SIGN IN</Link>
-        }
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT{' '}
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            {' '}
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
       </div>
+      <CartDropdown />
     </div>
   );
 };
@@ -34,7 +42,7 @@ const mapStateToProps = (state) => ({
   //this function should return an object
   //the property should be the value we pass in the component and the value the value of it.
   //In this case in this function we want to get the state object. The state here is the root reducer
-  currentUser: state.user.currentUser
-})
+  currentUser: state.user.currentUser,
+});
 
 export default connect(mapStateToProps)(Header);
