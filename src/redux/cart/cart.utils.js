c3e67bf915d1cac535dcbf7c8ed36343ea768774
Francisco Martins <fmaraposo@gmail.com>
2021-01-29 12:27:33 +0000

    adding multiple items to cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    //Checking if the item we are adding already exists in the Array
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id )
+
+    if(existingCartItem) {
+        //if it exists we will create a new array by mapping the original array
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+            )
+    }
+
+    return [...cartItems, {...cartItemToAdd, quantity:1}]
+}
