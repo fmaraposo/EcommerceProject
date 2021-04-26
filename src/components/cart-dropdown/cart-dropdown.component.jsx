e43f3d35fc0b953a3e122ac0b1099b9c71742007
Francisco Martins <fmaraposo@gmail.com>
2021-04-26 17:06:06 +0100

    added checkout page, checkout item component and updated cart to use reselectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component'
 
 import {selectCartItems} from '../../redux/cart/cart.selectors';
+import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {cartItems.length ? (
@@ -19,6 +20,7 @@ const CartDropdown = ({cartItems, history}) => (
             }
         </div>
         <CustomButton onClick={()=> {
+            dispatch(toggleCartHidden())
             history.push('/checkout')
         }}>
         GO TO CHECKOUT
@@ -31,5 +33,6 @@ const mapStateToProps = (state) => {
         cartItems: selectCartItems(state)
     }
 }
+
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
